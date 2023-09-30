03a9362138c669dd2afdfeffe279a668765e8df6
JEONGSSUNI <980916chung@naver.com>
2023-09-30 22:01:17 +0900

    step_6

---
+++ step_6/no_1316.py
@@ -0,0 +1,13 @@
+n = int(input())
+count = n
+for i in range(n) :
+    word = input()
+    for s in range(len(word)-1) :
+        if word[s] == word[s+1] :
+            continue
+        else :
+            continue
+print(count)
+
+
+# 다시 풀기
