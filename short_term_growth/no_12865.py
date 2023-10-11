3b9aae1cdd6c8734a68d63d2c10d19e1452e7b10
JEONGSSUNI <980916chung@naver.com>
2023-10-11 17:20:27 +0900

    DP 알고리즘 이해 필요

---
+++ short_term_growth/no_12865.py
@@ -0,0 +1,30 @@
+# # 틀림
+# n, k = map(int, input().split())
+#
+# thing_list = []
+# for i in range(n) :
+#     thing_list.append(list(map(int, input().split())))
+#
+# max_w = 0
+# for i in range(0, n) :
+#     j = 0
+#     while j < n :
+#         if i == j or i < j :
+#             j += 1
+#             break
+#         if thing_list[i][0] + thing_list[j][0] <= k :
+#             if max_w < thing_list[i][1] + thing_list[j][1] :
+#                 max_w = thing_list[i][1] + thing_list[j][1]
+#         j += 1
+#
+# print(max_w)
+
+# 동적 프로그래밍 (DP)
+n, k = map(int, input().split())
+
+dp = [[0 for _ in range(n + 1)][0 for _ in range(k + 1)]]
+
+
+thing_list = []
+for i in range(n) :
+    thing_list.append(list(map(int, input().split())))
