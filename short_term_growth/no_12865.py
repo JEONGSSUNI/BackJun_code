82d1d1804112aecc92401913d28eff20e63c3d09
JEONGSSUNI <980916chung@naver.com>
2023-10-13 16:33:30 +0900

    우선순위 힙 알고리즘

--- short_term_growth/no_12865.py
+++ short_term_growth/no_12865.py
@@ -22,9 +22,19 @@
 # 동적 프로그래밍 (DP)
 n, k = map(int, input().split())
 
-# dp = [[0 for _ in range(n + 1)][0 for _ in range(k + 1)]]
+dp = [[0]*(k+1) for _ in range(n + 1)] # n행, k열의 2차원 배열 생성
+thing_list = [[0, 0]] # 왜 0번 인덱스에 [0, 0]을 넣어야하는가?
 
-
-thing_list = []
 for i in range(n) :
     thing_list.append(list(map(int, input().split())))
+
+for i in range(1, n+1) :
+    w = thing_list[i][0]
+    v = thing_list[i][1]
+
+    for j in range(1, k+1) :
+        if j < w :
+            dp[i][j] = dp[i-1][j]
+        else :
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
+print(dp[n][k])
